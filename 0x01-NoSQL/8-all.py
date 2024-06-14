43ac6984352b89159bbb071c6ad3ebb307dd5970
Odinaka Nnamani <nnamani.odinakarobert@gmail.com>
2024-06-15 00:52:36 +0100

    insert document in python

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -8,12 +8,13 @@ import pymongo
 def list_all(mongo_collection):
     """
      Lists all documents in a collection.
-    
+
     Args:
         mongo_collection: The pymongo collection object.
-    
+
     Returns:
-        A list of documents in the collection. Returns an empty list if no document is found.
+        A list of documents in the collection.
+        Returns an empty list if no document is found.
     """
     documents = list(mongo_collection.find())
     return documents
